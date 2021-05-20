863e75553021ba194cfec06546fa6e600a030a29
seikhou <seikhoudanfakha@gmail.com>
2021-05-20 09:17:37 +0000

    Initial commit

---
+++ DemoGitProject/src/main/java/com/saraya/DemoGitProject/DemoGitProjectApplication.java
@@ -0,0 +1,13 @@
+package com.saraya.DemoGitProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGitProjectApplication.class, args);
+	}
+
+}
---
+++ DemoGitProject/src/test/java/com/saraya/DemoGitProject/DemoGitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saraya.DemoGitProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
